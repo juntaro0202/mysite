fd39dd55b8874e99aeedb25f797a948f32456ee5
hayashi-3 <hayashi.gml@gmail.com>
2021-06-03 10:21:05 +0900

    [add]works

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,9 @@
     <v-col cols="12" sm="8" md="6">
       <div class="text-center">
         <main-logo />
+        <h2>じゅんちゃんとは？</h2>
+        <p>哲学を基により良い社会とは何か、より良い生き方とは何かを考えています。</p>
+        <p>youtubeで時事ネタを語ったり、読書会をしたり色々やってます。</p>
       </div>
     </v-col>
   </v-row>
---
+++ pages/youtube.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <h2>youtube</h2>
+    <p>
+      <ul>哲学入門チャンネル</ul>
+      <ul>時事ネタとか喋ってます。たまにライブしたり、会員さん限定の読書会もしてます。</ul>
+    </p>
+    <iframe width="424" height="238"
+      src="https://www.youtube.com/embed/cOPSH5VTFlw"
+      title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;
+      clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+  </v-container>
+</template>
